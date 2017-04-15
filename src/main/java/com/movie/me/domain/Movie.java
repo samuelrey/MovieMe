7117bb282cd689ed454e2acea472512ff01235db
Samuel Villavicencio <samuelrey010@gmail.com>
2017-04-14 21:22:53 -0700

    Implement MovieBuilder to simplify creation in the case that the model changes. Begin repository test refactor.

--- src/main/java/com/movie/me/domain/Movie.java
+++ src/main/java/com/movie/me/domain/Movie.java
@@ -4,152 +4,280 @@ import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
 
-@NodeEntity(label="Movie")
+@NodeEntity(label = "Movie")
 public class Movie {
-	@GraphId
-	private Long id;
+    @GraphId
+    private Long id;
 
-	@Property(name="imdbid")
-	private String imdbid;
-	
-	@Property(name="title")
-	private String title;
+    @Property(name = "imdbid")
+    private String imdbid;
 
-	@Property(name="rating")
-	private double rating;
-	
-	@Property(name="rated")
-	private String rated;
-	
-	@Property(name="released")
-	private int releaseDate;
+    @Property(name = "title")
+    private String title;
 
-	@Property(name="plot")
-	private String plot;
+    @Property(name = "rating")
+    private double rating;
 
-	@Property(name="writer")
-	private String writer;
+    @Property(name = "rated")
+    private String rated;
 
-	@Property(name="director")
-	private String director;
+    @Property(name = "released")
+    private int releaseDate;
 
-	@Property(name="actors")
-	private String actors;
+    @Property(name = "plot")
+    private String plot;
 
-	@Property(name="genre")
-	private String genre;
+    @Property(name = "writer")
+    private String writer;
 
-	@Property(name="runtime")
-	private int runtime;
+    @Property(name = "director")
+    private String director;
 
-	@Property(name="poster")
-	private String poster;
+    @Property(name = "actors")
+    private String actors;
 
-	public String getImdbid() {
-		return imdbid;
-	}
+    @Property(name = "genre")
+    private String genre;
 
-	public void setImdbid(String imdbid) {
-		this.imdbid = imdbid;
-	}
+    @Property(name = "runtime")
+    private int runtime;
 
-	public String getRated() {
-		return rated;
-	}
+    @Property(name = "poster")
+    private String poster;
 
-	public void setRated(String rated) {
-		this.rated = rated;
-	}
+    public String getImdbid() {
+        return imdbid;
+    }
 
-	public String getPlot() {
-		return plot;
-	}
+    public void setImdbid(String imdbid) {
+        this.imdbid = imdbid;
+    }
 
-	public void setPlot(String plot) {
-		this.plot = plot;
-	}
+    public String getRated() {
+        return rated;
+    }
 
-	public String getWriter() {
-		return writer;
-	}
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
 
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
+    public String getPlot() {
+        return plot;
+    }
 
-	public String getDirector() {
-		return director;
-	}
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
 
-	public void setDirector(String director) {
-		this.director = director;
-	}
+    public String getWriter() {
+        return writer;
+    }
 
-	public String getActors() {
-		return actors;
-	}
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
 
-	public void setActors(String actors) {
-		this.actors = actors;
-	}
+    public String getDirector() {
+        return director;
+    }
 
-	public String getGenre() {
-		return genre;
-	}
+    public void setDirector(String director) {
+        this.director = director;
+    }
 
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
+    public String getActors() {
+        return actors;
+    }
 
-	public int getRuntime() {
-		return runtime;
-	}
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
 
-	public void setRuntime(int runtime) {
-		this.runtime = runtime;
-	}
+    public String getGenre() {
+        return genre;
+    }
 
-	public String getPoster() {
-		return poster;
-	}
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
-	public void setPoster(String poster) {
-		this.poster = poster;
-	}
+    public int getRuntime() {
+        return runtime;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getPoster() {
+        return poster;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public double getRating() {
-		return rating;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setRating(double rating) {
-		this.rating = rating;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public int getReleaseDate() {
-		return releaseDate;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void setReleasedate(int releaseDate) {
-		this.releaseDate = releaseDate;
-	}
+    public double getRating() {
+        return rating;
+    }
 
-	public String toString() {
-		return this.title;
-	}
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleasedate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", imdbid='" + imdbid + '\'' +
+                ", title='" + title + '\'' +
+                ", rating=" + rating +
+                ", rated='" + rated + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", plot='" + plot + '\'' +
+                ", writer='" + writer + '\'' +
+                ", director='" + director + '\'' +
+                ", actors='" + actors + '\'' +
+                ", genre='" + genre + '\'' +
+                ", runtime=" + runtime +
+                ", poster='" + poster + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (Double.compare(movie.rating, rating) != 0) return false;
+        if (releaseDate != movie.releaseDate) return false;
+        if (runtime != movie.runtime) return false;
+        if (imdbid != null ? !imdbid.equals(movie.imdbid) : movie.imdbid != null) return false;
+        if (title != null ? !title.equals(movie.title) : movie.title != null) return false;
+        if (rated != null ? !rated.equals(movie.rated) : movie.rated != null) return false;
+        if (plot != null ? !plot.equals(movie.plot) : movie.plot != null) return false;
+        if (writer != null ? !writer.equals(movie.writer) : movie.writer != null) return false;
+        if (director != null ? !director.equals(movie.director) : movie.director != null) return false;
+        if (actors != null ? !actors.equals(movie.actors) : movie.actors != null) return false;
+        if (genre != null ? !genre.equals(movie.genre) : movie.genre != null) return false;
+        return poster != null ? poster.equals(movie.poster) : movie.poster == null;
+    }
+
+    public static class MovieBuilder {
+        private String imdbid;
+        private String title;
+        private double rating;
+        private String rated;
+        private int releaseDate;
+        private String plot;
+        private String writer;
+        private String director;
+        private String actors;
+        private String genre;
+        private int runtime;
+        private String poster;
+
+        public Movie build() {
+            Movie movie = new Movie();
+            movie.setTitle(title);
+            movie.setActors(actors);
+            movie.setImdbid(imdbid);
+            movie.setRated(rated);
+            movie.setRating(rating);
+            movie.setReleasedate(releaseDate);
+            movie.setPlot(plot);
+            movie.setWriter(writer);
+            movie.setDirector(director);
+            movie.setGenre(genre);
+            movie.setRuntime(runtime);
+            movie.setPoster(poster);
+            return movie;
+        }
+
+        public MovieBuilder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public MovieBuilder withImdbid(String imdbid) {
+            this.imdbid = imdbid;
+            return this;
+        }
+
+        public MovieBuilder withRating(double rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public MovieBuilder withRated(String rated) {
+            this.rated = rated;
+            return this;
+        }
+
+        public MovieBuilder withReleaseDate(int releaseDate) {
+            this.releaseDate = releaseDate;
+            return this;
+        }
+
+        public MovieBuilder withPlot(String plot) {
+            this.plot = plot;
+            return this;
+        }
+
+        public MovieBuilder withWriter(String writer) {
+            this.writer = writer;
+            return this;
+        }
+
+        public MovieBuilder withDirector(String director) {
+            this.director = director;
+            return this;
+        }
+
+        public MovieBuilder withActors(String actors) {
+            this.actors = actors;
+            return this;
+        }
+
+        public MovieBuilder withGenre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public MovieBuilder withRuntime(int runtime) {
+            this.runtime = runtime;
+            return this;
+        }
+
+        public MovieBuilder withPoster(String poster) {
+            this.poster = poster;
+            return this;
+        }
+    }
 }
--- src/test/java/com/movie/me/repository/MovieRepositoryTest.java
+++ src/test/java/com/movie/me/repository/MovieRepositoryTest.java
@@ -2,20 +2,17 @@ package com.movie.me.repository;
 
 import com.movie.me.beans.Neo4jTestConfiguration;
 import com.movie.me.domain.Movie;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -25,35 +22,74 @@ public class MovieRepositoryTest {
     @Autowired
     MovieRepository movieRepository;
 
-    private Movie newHope;
+    //  findByImdbid
+    @Test
+    public void testFindByImdbidValid() {
+        Movie expected = new Movie.MovieBuilder()
+                .withTitle("Star Wars: Episode IV - A New Hope")
+                .withRated("PG")
+                .withActors("Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing")
+                .withDirector("George Lucas")
+                .withImdbid("tt0076759")
+                .withRating(8.7)
+                .withGenre("Action, Adventure, Fantasy")
+                .withRuntime(121)
+                .withWriter("George Lucas")
+                .withReleaseDate(233366400)
+                .withPoster("https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0O"
+                        + "TY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg")
+                .withPlot("Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to "
+                        + "save the galaxy from the Empire's world-destroying battle-station, while also attempting to "
+                        + "rescue Princess Leia from the evil Darth Vader.")
+                .build();
 
-    @Before
-    public void setup() {
-        newHope = movieRepository.findByImdbid("tt0076759");
+        Movie actual = movieRepository.findByImdbid("tt0076759");
+
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
-    @DirtiesContext
-    public void testFindByExistentActors() {
-        List<Movie> result = movieRepository.findByActorLike("Harrison Ford", 0, 10);
+    public void testFindByImdbidInvalid() {
+        Movie actual = movieRepository.findByImdbid("");
 
-        assertThat(result, hasItems(newHope));
+        assertThat(actual, equalTo(null));
+    }
+
+    //  findByActors
+    @Test
+    public void testFindByActorExist() {
+        List<Movie> expected = new ArrayList<>();
+        String[] imdbids = {"tt0076759", "tt0086190", "tt0083658", "tt0082971", "tt0087469", "tt0097576", "tt0367882",
+                "tt0080684", "tt0096463", "tt0118972"};
+        for(String imdbid: imdbids) {
+            expected.add(movieRepository.findByImdbid(imdbid));
+        }
+
+        List<Movie> actual = movieRepository.findByActorLike("Harrison Ford", 0, 10);
+
+        assertThat(actual, contains(expected.toArray()));
     }
 
     @Test
-    @DirtiesContext
-    public void testFindByNonexistentActors() {
-        List<Movie> result = movieRepository.findByActorLike("Harrison Chevy", 1, 10);
+    public void testFindByActorNotExist() {
+        List<Movie> actual = movieRepository.findByActorLike("Harrison Chevy", 0, 10);
 
-        assertThat(result.isEmpty(), is(true));
+        assertThat(actual.isEmpty(), is(true));
     }
 
     @Test
-    @DirtiesContext
-    public void testFindByActorsCaseInsensitive() {
-        List<Movie> lowerCase = movieRepository.findByActorLike("harrison ford", 1, 10);
-        List<Movie> upperCase = movieRepository.findByActorLike("HARRISON FORD", 1, 10);
+    public void testFindByActorCaseInsensitive() {
+        List<Movie> lowerCase = movieRepository.findByActorLike("harrison ford", 0, 10);
+        List<Movie> upperCase = movieRepository.findByActorLike("HARRISON FORD", 0, 10);
 
         assertThat(lowerCase, equalTo(upperCase));
+    }
+
+    @Test
+    public void testFindByActorNextPageUniqueMovies() {
+        List<Movie> pageOne = movieRepository.findByActorLike("harrison ford", 0, 10);
+        List<Movie> pageTwo = movieRepository.findByActorLike("harrison ford", 10, 10);
+
+        assertThat(pageOne, not(containsInAnyOrder(pageTwo)));
     }
 }
